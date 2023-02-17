3dbe0cfaaf34e7b3962f8a8685527f94dfb06472
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-02-17 18:15:39 +0200

    removed unused API calls, re-written egenerate function

---
+++ classes/Service/PdfRegenerator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Service\UcrmApi;
+
+chdir(__DIR__);
+
+class PdfRegenerator
+{
+    /**
+     * @var UcrmApi
+     */
+    private $ucrmApi;
+
+    public function __construct(UcrmApi $ucrmApi)
+    {
+        $this->ucrmApi = new UcrmApi();
+    }
+
+    public function generateView(array $invoices): void
+    {
+        $renderer = new TemplateRenderer();
+        $renderer->render(
+            __DIR__ .
+                '/../../templates/pdf-regenerator.php',
+            [
+                'invoices' => $invoices,
+            ]
+        );
+    }
+
+    public function regeneratePdf(int $invoiceId): void
+    {
+        try {
+            $this->ucrmApi::doRequest("invoices/$invoiceId/regenerate-pdf", 'PATCH');
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
+}
